626d161554ad2965fd52c5a2e36eaccd7dbf260b
Mo Sharif <mo@mosh-media.com>
2019-06-02 22:11:53 -0700

    Added Ant Design components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
+import { Component, OnInit } from "@angular/core";
+import { Store, select } from "@ngrx/store";
 
-import { IAppState } from './store/state/app.state';
-import { GetConfig } from './store/actions/config.actions';
-import { selectConfig } from './store/selectors/config.selector';
+import { IAppState } from "./store/state/app.state";
+import { GetConfig } from "./store/actions/config.actions";
+import { selectConfig } from "./store/selectors/config.selector";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  title = 'AngularX';
-  config$ = this._store.pipe(select(selectConfig));
+	title = "AngularX";
+	config$ = this._store.pipe(select(selectConfig));
+	copyright = "AngularX ©2019 ❤️";
+	constructor(private _store: Store<IAppState>) {}
 
-  constructor(private _store: Store<IAppState>) {}
-
-  ngOnInit() {
-    this._store.dispatch(new GetConfig());
-  }
+	ngOnInit() {
+		this._store.dispatch(new GetConfig());
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,59 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
 
-import { appReducers } from './store/reducers/app.reducers';
-import { environment } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';
-import { ConfigEffects } from './store/effects/config.effects';
-import { UserEffects } from './store/effects/user.effects';
-import { PostEffects } from './store/effects/post.effects';
-import { AppComponent } from './app.component';
-import { UserService } from './services/user.service';
-import { PostService } from './services/post.service';
-import { UsersComponent as UsersContainerComponent } from './containers/users/users.component';
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './containers/user/user.component';
-import { UserDetailsComponent } from './components/user-details/user-details.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostsComponent as PostsComponentContainer } from './containers/posts/posts.component';
-import { MaterialModule } from './material/material.module';
-import { HeaderComponent } from './components/shared/header/header/header.component';
+import { appReducers } from "./store/reducers/app.reducers";
+import { environment } from "../environments/environment";
+import { AppRoutingModule } from "./app-routing.module";
+import { ConfigEffects } from "./store/effects/config.effects";
+import { UserEffects } from "./store/effects/user.effects";
+import { PostEffects } from "./store/effects/post.effects";
+import { AppComponent } from "./app.component";
+import { UserService } from "./services/user.service";
+import { PostService } from "./services/post.service";
+import { UsersComponent as UsersContainerComponent } from "./containers/users/users.component";
+import { UsersComponent } from "./components/users/users.component";
+import { UserComponent } from "./containers/user/user.component";
+import { UserDetailsComponent } from "./components/user-details/user-details.component";
+import { PostsComponent } from "./components/posts/posts.component";
+import { PostsComponent as PostsComponentContainer } from "./containers/posts/posts.component";
+import { MaterialModule } from "./material/material.module";
+import { HeaderComponent } from "./components/shared/header/header.component";
+import { NgZorroAntdModule, NZ_I18N, en_US } from "ng-zorro-antd";
+import { FooterComponent } from "./components/shared/footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UsersContainerComponent,
-    UsersComponent,
-    UserComponent,
-    UserDetailsComponent,
-    PostsComponent,
-    PostsComponentContainer,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    HttpClientModule,
-    StoreModule.forRoot(appReducers),
-    EffectsModule.forRoot([UserEffects, ConfigEffects, PostEffects]),
-    StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    AppRoutingModule
-  ],
-  providers: [UserService, PostService],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		UsersContainerComponent,
+		UsersComponent,
+		UserComponent,
+		UserDetailsComponent,
+		PostsComponent,
+		PostsComponentContainer,
+		HeaderComponent,
+		FooterComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MaterialModule,
+		HttpClientModule,
+		StoreModule.forRoot(appReducers),
+		EffectsModule.forRoot([UserEffects, ConfigEffects, PostEffects]),
+		StoreRouterConnectingModule.forRoot({ stateKey: "router" }),
+		!environment.production ? StoreDevtoolsModule.instrument() : [],
+		AppRoutingModule,
+		/** import ng-zorro-antd root module，you should import NgZorroAntdModule and avoid importing sub modules directly **/
+		NgZorroAntdModule
+	],
+	providers: [{ provide: NZ_I18N, useValue: en_US }, UserService, PostService],
+	bootstrap: [AppComponent]
 })
 export class AppModule {}
