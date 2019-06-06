6d22570e0495bd32ba96de939f144b687a00862b
Mo Sharif <mohammed.sharif@bigfishgames.com>
2019-06-06 16:35:50 -0700

    Cleaned up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,15 @@ import { PostsComponent } from "./containers/posts/posts.component";
 import { SettingsComponent } from "./components/settings/settings.component";
 import { CalendarComponent } from "./components/calendar/calendar.component";
 import { HomeComponent } from "./components/home/home.component";
-
+// TODO: https://ng.ant.design/components/breadcrumb/en
+// Change to use children routes
 const routes: Routes = [
   { path: "home", component: HomeComponent, data: { breadcrumb: "Home" } },
-  { path: "users", component: UsersComponent, data: { breadcrumb: "Users" } },
-  { path: "posts", component: PostsComponent, data: { breadcrumb: "Posts" }  },
-  { path: "calendar", component: CalendarComponent, data: { breadcrumb: "Calendar" }  },
-  { path: "settings", component: SettingsComponent, data: { breadcrumb: "Settings" }  },
-  { path: "user/:id", component: UserComponent, data: { breadcrumb: "User" }  },
+  { path: "home/users", component: UsersComponent, data: { breadcrumb: "Users" } },
+  { path: "home/posts", component: PostsComponent, data: { breadcrumb: "Posts" }  },
+  { path: "home/calendar", component: CalendarComponent, data: { breadcrumb: "Calendar" }  },
+  { path: "home/settings", component: SettingsComponent, data: { breadcrumb: "Settings" }  },
+  { path: "home/user/:id", component: UserComponent, data: { breadcrumb: "User" }  },
   { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
 
