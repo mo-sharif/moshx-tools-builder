ad02a6fcd587af4c0bc40570fce5c4d0833a4951
Mo Sharif <mohammed.sharif@bigfishgames.com>
2019-06-12 17:10:51 -0700

    Created Auth Service
    Moved some logic from auth effects to auth service.. signing in with google works partial.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,19 +3,26 @@ import { AngularFireAuth } from "@angular/fire/auth";
 import * as firebase from "firebase/app";
 
 @Injectable({
-	providedIn: "root"
+  providedIn: "root"
 })
 export class AuthService {
-	constructor(public afAuth: AngularFireAuth) {}
+  constructor(public afAuth: AngularFireAuth) {}
 
-	doGoogleLogin() {
-		return new Promise<any>((resolve, reject) => {
-			let provider = new firebase.auth.GoogleAuthProvider();
-			provider.addScope("profile");
-			provider.addScope("email");
-			this.afAuth.auth.signInWithPopup(provider).then(res => {
-				resolve(res);
-			});
-		});
-	}
+  public googleLoginPromise() {
+    return new Promise<any>((resolve, reject) => {
+      let provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope("profile");
+      provider.addScope("email");
+      this.afAuth.auth.signInWithPopup(provider).then(res => {
+        resolve(res);
+      });
+    });
+  }
+  public signOut() {
+    return this.afAuth.auth.signOut();
+  }
+
+  public authState() {
+    return this.afAuth.authState;
+  }
 }
--- src/app/store/effects/auth.effects.ts
+++ src/app/store/effects/auth.effects.ts
@@ -1,103 +1,84 @@
+import { AuthService } from '../../services/auth.service';
 import { Injectable } from "@angular/core";
 import { Effect, ofType, Actions } from "@ngrx/effects";
 import { Store, select } from "@ngrx/store";
 import { of, from } from "rxjs";
 import {
-	switchMap,
-	map,
-	withLatestFrom,
-	delay,
-	catchError
+  switchMap,
+  map,
+  withLatestFrom,
+  delay,
+  catchError
 } from "rxjs/operators";
 import { Observable } from "rxjs";
-import { AngularFireAuth } from "@angular/fire/auth";
-import * as firebase from "firebase/app";
-
 import { IAppState } from "../state/app.state";
 import {
-	EAuthActions,
-	GetUser,
-	Authenticated,
-	NotAuthenticated,
-	GoogleLogin,
-	Logout,
-	AuthError
+  EAuthActions,
+  GetUser,
+  Authenticated,
+  NotAuthenticated,
+  GoogleLogin,
+  Logout,
+  AuthError
 } from "../actions/auth.actions";
 import { User } from "../../models/user.interface";
 import { selectCurrentUser } from "../selectors/auth.selectors";
 
 @Injectable()
 export class AuthEffects {
-	@Effect()
-	getUser$ = this._actions$.pipe(
-		ofType<GetUser>(EAuthActions.GetUser),
-		map(action => {
-			action.payload;
-		}),
-		withLatestFrom(this._store.pipe(select(selectCurrentUser))),
-		switchMap(payload => this.afAuth.authState),
-		delay(200),
-		map(authData => {
-			if (authData) {
-				const user = new User(authData.uid, authData.displayName);
-				return new Authenticated(user);
-			} else {
-				// CHANGE ME: Use initial state instead
-				const user = new User(null, 'Guest');
-				return new NotAuthenticated(user);
-			}
-		}),
-		catchError(err => of(new AuthError()))
-	);
-
-	@Effect()
-	googleLogin$ = this._actions$.pipe(
-		ofType<GoogleLogin>(EAuthActions.GoogleLogin),
-		map(action => {
-			action.payload;
-		}),
-		switchMap(payload => {
-			// CHANGE ME: this action can't be dispatched
-			return from(this.googleLoginPromise());
-		})
-	);
-
-	@Effect()
-	logout$ = this._actions$.pipe(
-		ofType<Logout>(EAuthActions.Logout),
-		map(action => {
-			action.payload;
-		}),
-		switchMap(payload => {
-			return from(this.afAuth.auth.signOut());
-		}),
-		map(authData => {
-			return new NotAuthenticated();
-		}),
-		catchError(err => of(new AuthError()))
-	);
-
-	private googleLogin() {
-		const provider = new firebase.auth.GoogleAuthProvider();
-		return this.afAuth.auth.signInWithPopup(provider);
-    }
-
-    private googleLoginPromise(){
-        return new Promise<any>((resolve, reject) => {
-          let provider = new firebase.auth.GoogleAuthProvider();
-          provider.addScope('profile');
-          provider.addScope('email');
-          this.afAuth.auth
-          .signInWithPopup(provider)
-          .then(res => {
-            resolve(res);
-          })
-        })
+  @Effect()
+  getUser$ = this._actions$.pipe(
+    ofType<GetUser>(EAuthActions.GetUser),
+    map(action => {
+      action.payload;
+    }),
+    withLatestFrom(this._store.pipe(select(selectCurrentUser))),
+    switchMap(payload => this.authService.authState()),
+    delay(200),
+    map(authData => {
+      if (authData) {
+        const user = new User(authData.uid, authData.displayName);
+        return new Authenticated(user);
+      } else {
+        // CHANGE ME: Use initial state instead
+        const user = new User(null, "Guest");
+        return new NotAuthenticated(user);
       }
+    }),
+    catchError(err => of(new AuthError()))
+  );
 
-	constructor(
-		private afAuth: AngularFireAuth,
-		private _actions$: Actions,
-		private _store: Store<IAppState>
-	) {}
+  @Effect()
+  googleLogin$ = this._actions$.pipe(
+    ofType<GoogleLogin>(EAuthActions.GoogleLogin),
+    map(action => {
+      action.payload;
+    }),
+    switchMap(payload => {
+			// CHANGE ME: this action can't be dispatched
+			return this.authService.googleLoginPromise();
+      // return from();
+    })
+  );
+
+  @Effect()
+  logout$ = this._actions$.pipe(
+    ofType<Logout>(EAuthActions.Logout),
+    map(action => {
+      action.payload;
+    }),
+    switchMap(payload => {
+      return from(this.authService.signOut());
+    }),
+    map(authData => {
+      return new NotAuthenticated();
+    }),
+    catchError(err => of(new AuthError()))
+  );
+
+  constructor(
+    private authService: AuthService,
+    private _actions$: Actions,
+    private _store: Store<IAppState>
+  ) {}
 }
