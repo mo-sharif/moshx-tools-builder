2e49cd0250c4b1152a15c1b76c900488f1570ee0
Mo Sharif <mohammed.sharif@bigfishgames.com>
2019-06-17 17:07:24 -0700

    Small fix

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -3,30 +3,39 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { listStagger } from "../../animations/list-stagger.animation";
 
 @Component({
-	selector: "app-form",
-	templateUrl: "./form.component.html",
-	styleUrls: ["./form.component.css"],
-	animations: [listStagger] // register the animation
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.css"],
+  animations: [listStagger] // register the animation
 })
 export class AddUserComponent implements OnInit {
-	userForm: FormGroup;
+  userForm: FormGroup;
+  isLoadingOne = false;
+  fakeDisabled = true;
 
-	@Output() formReady = new EventEmitter<FormGroup>();
+  @Output() formReady = new EventEmitter<FormGroup>();
 
-	submitForm(): void {
-		for (const i in this.userForm.controls) {
-			this.userForm.controls[i].markAsDirty();
-			this.userForm.controls[i].updateValueAndValidity();
-		}
-		this.formReady.emit(this.userForm);
-	}
-	constructor(private fb: FormBuilder) {}
+  submitForm(): void {
+    for (const i in this.userForm.controls) {
+      this.userForm.controls[i].markAsDirty();
+      this.userForm.controls[i].updateValueAndValidity();
+    }
+    this.formReady.emit(this.userForm);
+  }
+  constructor(private fb: FormBuilder) {}
 
-	ngOnInit(): void {
-		this.userForm = this.fb.group({
-			name: [null, [Validators.required]],
-			city: [null, [Validators.required]],
-			job: [null, [Validators.required]]
-		});
-	}
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: [null, [Validators.required]],
+      city: [null, [Validators.required]],
+      job: [null, [Validators.required]]
+    });
+  }
+  fakeLoading($event): void {
+    this.fakeDisabled = false;
+    this.isLoadingOne = true;
+    setTimeout(() => {
+      this.isLoadingOne = false;
+    }, 5000);
+  }
 }
