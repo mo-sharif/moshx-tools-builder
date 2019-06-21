4f3003e6e371ad5d14a99c2483f0c58f304b642f
Mo Sharif <mo@mosh-media.com>
2019-06-21 08:26:34 -0700

    Added Drag and drop support

--- src/app/containers/playground/playground.component.ts
+++ src/app/containers/playground/playground.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+	CdkDragDrop,
+	moveItemInArray,
+	transferArrayItem
+} from "@angular/cdk/drag-drop";
 
 @Component({
-  selector: 'app-playground',
-  templateUrl: './playground.component.html',
-  styleUrls: ['./playground.component.css']
+	selector: "app-playground",
+	templateUrl: "./playground.component.html",
+	styleUrls: ["./playground.component.css"]
 })
 export class PlaygroundComponent implements OnInit {
+	constructor() {}
+	todo = ["Get to work", "Pick up groceries", "Go home", "Fall asleep"];
 
-  constructor() { }
+	done = ["Get up", "Brush teeth", "Take a shower", "Check e-mail", "Walk dog"];
 
-  ngOnInit() {
-  }
+	drop(event: CdkDragDrop<string[]>) {
+		if (event.previousContainer === event.container) {
+			moveItemInArray(
+				event.container.data,
+				event.previousIndex,
+				event.currentIndex
+			);
+		} else {
+			transferArrayItem(
+				event.previousContainer.data,
+				event.container.data,
+				event.previousIndex,
+				event.currentIndex
+			);
+		}
+	}
 
+	ngOnInit() {}
 }
--- src/app/containers/playground/playground.module.ts
+++ src/app/containers/playground/playground.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { PlaygroundRoutingModule } from './playground-routing.module';
 import { PlaygroundComponent } from './playground.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NzCardModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [PlaygroundComponent],
   imports: [
+    NzCardModule,
+    DragDropModule,
     CommonModule,
     PlaygroundRoutingModule
   ]
