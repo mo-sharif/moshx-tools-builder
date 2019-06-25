304a5566af88ef68955fc9e65ecab3dce4aca383
Mo Sharif <mo@mosh-media.com>
2019-06-25 07:53:03 -0700

    Google login button

---
+++ src/app/components/google-login-button/google-login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { IUser } from '../../models/user.interface';
+import { listStagger } from '../../animations/list-stagger.animation';
+
+@Component({
+  selector: 'google-login',
+  templateUrl: './google-login.component.html',
+  animations: [listStagger]
+})
+export class GoogleLoginComponent implements OnInit {
+  @Input()
+  user: IUser
+
+  @Output()
+  googleLoginEvent: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  logoutEvent: EventEmitter<any> = new EventEmitter();
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  googleLogin() { 
+    this.googleLoginEvent.emit()
+  }
+
+  logout() { 
+    this.logoutEvent.emit()
+  }
+
+}
