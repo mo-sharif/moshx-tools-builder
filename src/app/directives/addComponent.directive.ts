f4993854485d2cf4b8ec40b5d3fa12b60f056a89
Mo Sharif <mo@mosh-media.com>
2019-08-04 20:57:45 -0700

    Added Onchanges event for AddComponent directive

--- src/app/directives/addComponent.directive.ts
+++ src/app/directives/addComponent.directive.ts
@@ -1,23 +1,30 @@
-import { Directive, Type, ViewContainerRef, Input, ComponentFactoryResolver } from '@angular/core';
+import {
+	Directive,
+	Type,
+	ViewContainerRef,
+	Input,
+	ComponentFactoryResolver,
+	OnChanges
+} from "@angular/core";
 
 @Directive({
-  selector: '[add-comp]',
+	selector: "[add-comp]"
 })
-export class AddComponentDirective {
-
-  @Input('comp') comp : Type<any>
-  @Input('selectedProject') selectedProject : Type<any>
-  constructor(public viewContainerRef: ViewContainerRef,
-  public componentFactoryResolver:ComponentFactoryResolver) {
-    
-   }
-
-   ngAfterViewInit(){
-     Promise.resolve().then(()=>{
-     let componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.comp);
-     this.viewContainerRef.clear();
-     let cmpRef = this.viewContainerRef.createComponent(componentFactory);
-     cmpRef.instance.selectedProject = this.selectedProject
-     })
-   }
+export class AddComponentDirective implements OnChanges {
+	@Input("comp") comp: Type<any>;
+	@Input("selectedProject") selectedProject: Type<any>;
+	constructor(
+		public viewContainerRef: ViewContainerRef,
+		public componentFactoryResolver: ComponentFactoryResolver
+	) {}
+	ngOnChanges(): void {
+		Promise.resolve().then(() => {
+			let componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+				this.comp
+			);
+			this.viewContainerRef.clear();
+			let cmpRef = this.viewContainerRef.createComponent(componentFactory);
+			cmpRef.instance.selectedProject = this.selectedProject;
+		});
+	}
 }
