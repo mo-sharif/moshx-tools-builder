2626573ef64bf4853acc982b5a4d8f3c876a8fc0
Mo Sharif <mo@mosh-media.com>
2019-08-24 17:21:23 -0700

    Update edit-project.module.ts

--- src/app/containers/edit-project/edit-project.module.ts
+++ src/app/containers/edit-project/edit-project.module.ts
@@ -5,13 +5,11 @@ import { EditProjectComponent as EditProjectContainer } from "./edit-project.com
 import { EditProjectComponent } from "../../components/save-project/save-project.component";
 import { addComponentModule } from "src/app/directives/addComponent.module";
 import { ComponentsModule } from "src/app/custom/components-module";
-import { ShortFormComponent } from "../../components/forms/short-form/short-form.component";
 
 @NgModule({
   declarations: [
     EditProjectContainer,
     EditProjectComponent,
-    ShortFormComponent,
   ],
   imports: [
     ComponentsModule,
