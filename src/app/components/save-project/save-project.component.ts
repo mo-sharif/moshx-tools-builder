ba5f8468ef998a8851e0959720b8c47b7d35d582
Mo Sharif <mo@mosh-media.com>
2019-08-25 02:21:19 -0700

    Update save-project.component.ts

--- src/app/components/save-project/save-project.component.ts
+++ src/app/components/save-project/save-project.component.ts
@@ -1,4 +1,11 @@
-import { Component, Output, Input, EventEmitter, OnInit } from "@angular/core";
+import {
+	Component,
+	Output,
+	Input,
+	EventEmitter,
+	OnInit,
+	OnChanges
+} from "@angular/core";
 
 import {
 	FormBuilder,
@@ -10,14 +17,14 @@ import {
 import { Observable, Observer, of } from "rxjs";
 import { IProject } from "src/app/models/project.interface";
 import { IUser } from "src/app/models/user.interface";
-import { map, catchError } from "rxjs/operators";
+import { map, catchError, tap, delay } from "rxjs/operators";
 
 @Component({
 	selector: "app-save-project",
 	templateUrl: "./save-project.component.html",
 	styleUrls: ["./save-project.component.css"]
 })
-export class EditProjectComponent implements OnInit {
+export class EditProjectComponent implements OnInit, OnChanges {
 	@Output()
 	formData: EventEmitter<any> = new EventEmitter();
 
@@ -25,17 +32,18 @@ export class EditProjectComponent implements OnInit {
 	selectLoggedInUser: IUser;
 
 	@Input()
-	selectedProject$: Observable<IProject>;
+	selectProject: IProject;
 
 	@Input()
-	componentSettings: IProject["ComponentSettings"];
+	componentConfigs: IProject["componentConfigs"];
 
-	validateForm: FormGroup;
+	projectFrom: FormGroup;
+
 	submitForm = ($event: any, value: IProject) => {
 		$event.preventDefault();
-		for (const key in this.validateForm.controls) {
-			this.validateForm.controls[key].markAsDirty();
-			this.validateForm.controls[key].updateValueAndValidity();
+		for (const key in this.projectFrom.controls) {
+			this.projectFrom.controls[key].markAsDirty();
+			this.projectFrom.controls[key].updateValueAndValidity();
 		}
 		this.emitFormData(value);
 	};
@@ -57,51 +65,29 @@ export class EditProjectComponent implements OnInit {
 	confirmValidator = (control: FormControl): { [s: string]: boolean } => {
 		if (!control.value) {
 			return { required: true };
-		} else if (control.value !== this.validateForm.controls.password.value) {
+		} else if (control.value !== this.projectFrom.controls.password.value) {
 			return { confirm: true, error: true };
 		}
 		return {};
 	};
 
 	constructor(private fb: FormBuilder) {}
-	/* Sometimes profile comes in as null so we need to catch that */
 
 	ngOnInit() {
-		this.validateForm = this.fb.group({
-			profile: [
-				this.selectLoggedInUser.profile,
-				[Validators.required],
-				[this.titleAsyncValidator]
-			],
+		this.projectFrom = this.fb.group({
+			profile: [null, [Validators.required], [this.titleAsyncValidator]],
 			title: [null, [Validators.required]],
 			type: [null],
-			ComponentSettings: this.fb.group({
+			componentConfigs: this.fb.group({
 				httpRequestUrl: [null],
 				collectionUrl: [null]
 			})
 		});
-
-		this.selectedProject$
-			.pipe(
-				map((project: IProject) => {
-					this.validateForm.patchValue({
-						title: project.title,
-						type: project.type,
-						ComponentSettings: {
-							httpRequestUrl: project.ComponentSettings.httpRequestUrl,
-							collectionUrl: project.ComponentSettings.collectionUrl
-						}
-					});
-				}),
-				catchError(error => of(error))
-			)
-			.subscribe();
+		this.projectFrom.patchValue({ ...this.selectLoggedInUser});
 	}
-
-	/* 	formConfigs = () => {
-		return {
-			httpRequestUrl: [this.selectedProject.ComponentSettings.httpRequestUrl],
-			collectionUrl: [this.selectedProject.ComponentSettings.httpRequestUrl]
+	ngOnChanges() {
+		if (this.selectProject) {
+			this.projectFrom.patchValue({ ...this.selectProject });
 		}
-	}; */
+	}
 }
