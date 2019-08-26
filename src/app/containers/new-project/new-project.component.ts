efa5627df3b9abdc4288618125eedb2c53d10eda
Mo Sharif <mo@mosh-media.com>
2019-08-25 22:02:45 -0700

    Update new-project.component.ts

--- src/app/containers/new-project/new-project.component.ts
+++ src/app/containers/new-project/new-project.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { listStagger } from "../../animations/list-stagger.animation";
 
+import { NewProject } from "../../store/actions/project.actions";
 import { Store, select } from "@ngrx/store";
 import { IAppState } from "../../store/state/app.state";
 import { selectContainers } from "../../store/selectors/config.selector";
+import { IProject, Project } from "src/app/models/project.interface";
 import { NavigateToRoute } from "src/app/store/actions/config.actions";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
 	selector: "app-new-project",
@@ -15,10 +18,17 @@ import { NavigateToRoute } from "src/app/store/actions/config.actions";
 export class NewProjectComponent implements OnInit {
 	containers$ = this._store.pipe(select(selectContainers));
 
-	constructor(private _store: Store<IAppState>) {}
+	constructor(private _store: Store<IAppState>, private _router: ActivatedRoute) {}
 
 	ngOnInit() {}
 	navigateToProject(type: string) {
+		this.dispatchNewProject(type);
 		this._store.dispatch(new NavigateToRoute(["home", "new-project", type]));
 	}
+
+	dispatchNewProject = (type) => {
+		let project = new Project("New Project", "NOT_YET_ASSIGNED", type);
+		this._store.dispatch(new NewProject(project));
+	};
+	
 }
