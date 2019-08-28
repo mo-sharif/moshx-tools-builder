dcb8350844adaf122df0b59014dac2c9773d6efe
Mo Sharif <mohammed.sharif@bigfishgames.com>
2019-08-28 15:02:49 -0700

    Update table.component.ts

--- src/app/custom/table/table.component.ts
+++ src/app/custom/table/table.component.ts
@@ -6,7 +6,7 @@ import { map, switchMap, distinctUntilChanged } from "rxjs/operators";
 
 @Injectable()
 export class RandomUserService {
-	randomUserUrl = "https://api.randomuser.me/";
+  randomUserUrl = "https://api.randomuser.me/";
 
 	getTableData(
 		pageIndex: number = 1,
@@ -14,98 +14,88 @@ export class RandomUserService {
 		sortField: string,
 		sortOrder: string,
 		genders: string[],
-		project: IProject
-	): Observable<{}> {
-		let params = new HttpParams()
-			.append("page", `${pageIndex}`)
-			.append("results", `${pageSize}`)
-			.append("sortField", sortField)
-			.append("sortOrder", sortOrder);
-		genders.forEach(gender => {
-			params = params.append("gender", gender);
-		});
-		return this.http.get(
-			`${project.componentConfigs.httpRequestUrl || this.randomUserUrl}`,
-			{
-				params
-			}
-		);
-	}
+		componentConfigs: IProject['componentConfigs']
+  ): Observable<{}> {
+    let params = new HttpParams()
+      .append("page", `${pageIndex}`)
+      .append("results", `${pageSize}`)
+      .append("sortField", sortField)
+      .append("sortOrder", sortOrder);
+    genders.forEach(gender => {
+      params = params.append("gender", gender);
+    });
+    return this.http.get(
+      `${componentConfigs.httpRequestUrl || this.randomUserUrl}`,
+      {
+        params
+      }
+    );
+  }
 
-	constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 }
 
 @Component({
-	selector: "app-table",
-	providers: [RandomUserService],
-	templateUrl: "./table.component.html",
-	styleUrls: ["./table.component.css"]
+  selector: "app-table",
+  providers: [RandomUserService],
+  templateUrl: "./table.component.html",
+  styleUrls: ["./table.component.css"]
 })
 export class TableComponent implements OnInit {
 	@Input()
-	selectProject$: Observable<IProject>;
+	componentConfigs: IProject['componentConfigs'];
 
-	pageIndex = 1;
-	pageSize = 10;
-	total = 1;
-	listOfData = [];
-	loading = true;
-	sortValue: string | null = null;
-	sortKey: string | null = null;
-	filterGender = [
-		{ text: "male", value: "male" },
-		{ text: "female", value: "female" }
-	];
-	searchGenderList: string[] = [];
+  pageIndex = 1;
+  pageSize = 10;
+  total = 1;
+  listOfData = [];
+  loading = true;
+  sortValue: string | null = null;
+  sortKey: string | null = null;
+  filterGender = [
+    { text: "male", value: "male" },
+    { text: "female", value: "female" }
+  ];
+  searchGenderList: string[] = [];
 
-	sort(sort: { key: string; value: string }): void {
-		this.sortKey = sort.key;
-		this.sortValue = sort.value;
-		this.searchData();
-	}
+  sort(sort: { key: string; value: string }): void {
+    this.sortKey = sort.key;
+    this.sortValue = sort.value;
+    this.searchData();
+  }
 
-	constructor(private randomUserService: RandomUserService) {}
+  constructor(private randomUserService: RandomUserService) {}
 
 	searchData(reset: boolean = false): void {
-		if (reset) {
-			this.pageIndex = 1;
-		}
-		this.loading = true;
+    if (reset) {
+      this.pageIndex = 1;
+    }
+    this.loading = true;
 
-		this.selectProject$
-			.pipe(
-				map((project: IProject) => {
-					if (project && project.hasOwnProperty('componentConfigs')) {
-						/* This is being called multiple times when exiting the route and reentering */
-						this.randomUserService
-							.getTableData(
-								this.pageIndex,
-								this.pageSize,
-								this.sortKey!,
-								this.sortValue!,
-								this.searchGenderList,
-								project
-							)
-							.subscribe((data: any) => {
+    this.randomUserService
+      .getTableData(
+        this.pageIndex,
+        this.pageSize,
+        this.sortKey!,
+        this.sortValue!,
+        this.searchGenderList,
+        this.componentConfigs
+      )
+      .subscribe((data: any) => {
+        console.log(data);
+        this.loading = false;
+        this.total = 200;
+        this.listOfData = data.results;
+      });
+  }
 
-								console.log(data)
-								this.loading = false;
-								this.total = 200;
-								this.listOfData = data.results;
-							});
-					}
-				})
-			)
-			.subscribe();
-	}
-
-	updateFilter(value: string[]): void {
-		this.searchGenderList = value;
-		console.log('data')
-		this.searchData(true);
-	}
+  updateFilter(value: string[]): void {
+    this.searchGenderList = value;
+    console.log("data");
+    this.searchData(true);
+  }
 
 	ngOnInit(): void {
-		this.searchData();
-	}
+		this.searchData()
+  }
 }
