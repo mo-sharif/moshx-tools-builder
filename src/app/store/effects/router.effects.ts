6bca258e6016809718364ee515007765b542ef18
Mo Sharif <mo@mosh-media.com>
2019-08-29 22:17:43 -0700

    Update router.effects.ts

--- src/app/store/effects/router.effects.ts
+++ src/app/store/effects/router.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Effect, ofType, Actions } from "@ngrx/effects";
-import { switchMap, withLatestFrom, map } from "rxjs/operators";
+import { switchMap, withLatestFrom, map, tap } from "rxjs/operators";
 import { of } from "rxjs";
 import { Store, select } from "@ngrx/store";
 import { IAppState } from "../state/app.state";
@@ -12,17 +12,21 @@ import {
 	NavigateSuccess
 } from "../actions/config.actions";
 import { ROUTER_NAVIGATION, RouterNavigationAction } from "@ngrx/router-store";
-import { selectIsCollapsed } from "../selectors/config.selector";
 import { Router } from "@angular/router";
+import { GetSelectedProjectFromRoute } from "../actions/project.actions";
 
 @Injectable()
 export class routerEffects {
-	@Effect()
+	/* FINISH ME!! */
+	/* @Effect()
 	routeChange$ = this._actions$.pipe(
-		ofType(ROUTER_NAVIGATION),
-		withLatestFrom(this._store.pipe(select(selectIsCollapsed))),
-		switchMap(() => of(new CloseDrawer()))
-	);
+		ofType<RouterNavigationAction>(ROUTER_NAVIGATION),
+		switchMap((route)=> {
+			let profileName = route.payload.routerState.url.split('/')[1]
+			let projectName = route.payload.routerState.url.split('/')[3]
+			return of(new GetSelectedProjectFromRoute([profileName, projectName]))
+		})
+	); */
 
 	@Effect()
 	navigateToRoute$ = this._actions$.pipe(
