6485d9c2eb291bf4ff2b41ac5bc86dcf9e6bd13e
Mo Sharif <mo@mosh-media.com>
2019-09-16 23:12:44 -0700

    Update addComponent.directive.ts

--- src/app/directives/addComponent.directive.ts
+++ src/app/directives/addComponent.directive.ts
@@ -16,6 +16,7 @@ import {
 export class AddComponentDirective implements OnInit, OnDestroy {
 	@Input() comp: Type<any>;
 	@Input() selectProject$: Type<any>;
+	@Input() selectUiComponents$: Type<any>;
 
 	@Output() formData: EventEmitter<any> = new EventEmitter<any>();
 
@@ -33,6 +34,7 @@ export class AddComponentDirective implements OnInit, OnDestroy {
 				this.viewContainerRef.clear();
 				let cmpRef = this.viewContainerRef.createComponent(componentFactory);
 				cmpRef.instance.selectProject$ = this.selectProject$;
+				cmpRef.instance.selectUiComponents$ = this.selectUiComponents$;
 				cmpRef.instance.formData.subscribe((data) => this.formData.emit(data));
 			})
 			.catch(error => console.log(error));
