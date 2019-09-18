e0c839ab6bcded88ee5a822186cde3e7882f8daf
Mo Sharif <mo@mosh-media.com>
2019-09-17 21:42:20 -0700

    Update user.effects.ts

--- src/app/store/effects/user.effects.ts
+++ src/app/store/effects/user.effects.ts
@@ -29,6 +29,7 @@ import { IUser } from "../../models/user.interface";
 import { selectUserList } from "../selectors/user.selector";
 
 import { SetLoading } from "../actions/loading.actions";
+import { selectLoggedInUser } from "../selectors/auth.selectors";
 
 
 @Injectable()
@@ -71,16 +72,21 @@ export class UserEffects {
 	@Effect()
 	getUsers$ = this._actions$.pipe(
 		ofType<GetUsers>(EUserActions.GetUsers),
-		switchMap(() => {
-			return this._userService.getUserList().pipe(
-				publishReplay(1),
-				refCount(),
-				switchMap((users: IUser[]) => [
-					new GetUsersSuccess(users),
-					new SetLoading(false)
-				]),
-				catchError(err => of(new GetUsersError({ error: err.message })))
-			);
+		withLatestFrom(this._store.pipe(select(selectLoggedInUser))),
+		switchMap(([action, selectLoggedInUser]) => {
+			if (selectLoggedInUser && selectLoggedInUser.profile) {
+				return this._userService.getTeammates(selectLoggedInUser).pipe(
+					publishReplay(1),
+					refCount(),
+					switchMap((users: IUser[]) => [
+						new GetUsersSuccess(users),
+						new SetLoading(false)
+					]),
+					catchError(err => of(new GetUsersError({ error: err.message })))
+				);
+			} else {
+				return of(new GetUsersSuccess([]),new SetLoading(false))
+			}
 		})
 	);
 
