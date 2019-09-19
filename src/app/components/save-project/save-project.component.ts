d3673f3f9411f6c74d418d75a6e1adcf74ac2d51
Mo Sharif <mohammed.sharif@bigfishgames.com>
2019-09-19 11:17:07 -0700

    Improve project saving flow

--- src/app/components/save-project/save-project.component.ts
+++ src/app/components/save-project/save-project.component.ts
@@ -1,173 +1,193 @@
 import { Component, Output, Input, EventEmitter, OnInit } from "@angular/core";
 
 import {
-	FormBuilder,
-	FormControl,
-	FormGroup,
-	ValidationErrors,
-	Validators
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators
 } from "@angular/forms";
 import { Observable, Observer, of, from } from "rxjs";
 import { IProject } from "src/app/models/project.interface";
 import { IUser } from "src/app/models/user.interface";
 import { listStagger } from "src/app/animations/list-stagger.animation";
 import { ProfileService } from "src/app/services/profile/profile.service";
-import { switchMap, map, tap, distinctUntilChanged, debounce, debounceTime } from "rxjs/operators";
+import {
+  switchMap,
+  map,
+  tap,
+  distinctUntilChanged,
+  debounce,
+  debounceTime,
+  withLatestFrom
+} from "rxjs/operators";
 
 export interface Item {
-	id: number;
-	key: string;
-	value: string;
-	type: string;
+  id: number;
+  key: string;
+  value: string;
+  type: string;
 }
 
 @Component({
-	selector: "app-save-project",
-	templateUrl: "./save-project.component.html",
-	styleUrls: ["./save-project.component.css"],
-	animations: [listStagger]
+  selector: "app-save-project",
+  templateUrl: "./save-project.component.html",
+  styleUrls: ["./save-project.component.css"],
+  animations: [listStagger]
 })
 export class EditProjectComponent implements OnInit {
-	@Input()
-	selectLoggedInUser$: Observable<IUser>;
+  @Input()
+  selectLoggedInUser$: Observable<IUser>;
 
-	@Input()
-	selectProject$: Observable<IProject>;
+  @Input()
+  selectProject$: Observable<IProject>;
 
-	@Input()
-	componentConfigs: IProject["componentConfigs"];
+  @Input()
+  componentConfigs: IProject["componentConfigs"];
 
-	@Input()
-	selectUiComponents: any;
+  @Input()
+  selectUiComponents: any;
 
-	@Output()
-	formData: EventEmitter<any> = new EventEmitter();
+  @Output()
+  formData: EventEmitter<any> = new EventEmitter();
 
-	@Output()
-	deleteProject: EventEmitter<any> = new EventEmitter();
+  @Output()
+  deleteProject: EventEmitter<any> = new EventEmitter();
 
-	projectFrom: FormGroup;
+  projectFrom: FormGroup;
 
-	controls: Array<Item> = [];
+  controls: Array<Item> = [];
 
-	submitForm = ($event: any, value: IProject) => {
-		$event.preventDefault();
-		for (const key in this.projectFrom.controls) {
-			this.projectFrom.controls[key].markAsDirty();
-			this.projectFrom.controls[key].updateValueAndValidity();
-		}
-		this.emitFormData(value);
-	};
+  submitForm = ($event: any, value: IProject) => {
+    $event.preventDefault();
+    for (const key in this.projectFrom.controls) {
+      this.projectFrom.controls[key].markAsDirty();
+      this.projectFrom.controls[key].updateValueAndValidity();
+    }
+    this.emitFormData(value);
+  };
 
-	emitFormData = value => {
-		this.formData.emit(value);
-	};
+  emitFormData = value => {
+    this.formData.emit(value);
+  };
 
-	emitDeleteProject = value => {
-		this.deleteProject.emit(value);
-	};
+  emitDeleteProject = value => {
+    this.deleteProject.emit(value);
+  };
 
-	titleAsyncValidator = (control: FormControl) => {
-		return new Observable((observer: Observer<ValidationErrors | null>) => {
-			this._profileService.loadProfile(control.value).pipe(
-				distinctUntilChanged(),
-				debounceTime(500),
-				map(data => {
-					let isTaken = data.length ? true : false;
-					if (isTaken) {
-						observer.next({ error: true, duplicated: true });
-					} else {
-						observer.next(null);
-					}
-					observer.complete();
-				})
-			).subscribe()
-		});
-	};
+  titleAsyncValidator = (control: FormControl) => {
+    return new Observable((observer: Observer<ValidationErrors | null>) => {
+      this._profileService
+        .loadProfile(control.value)
+        .pipe(
+          distinctUntilChanged(),
+          debounceTime(500),
+          withLatestFrom(this.selectLoggedInUser$),
+          map(([data, user]) => {
+            if (user.profile) {
+              observer.next({ error: false, duplicated: false });
+            } else {
+              let isTaken = data.length ? true : false;
+              if (isTaken) {
+                observer.next({ error: true, duplicated: true });
+              } else {
+                observer.next(null);
+              }
+            }
 
-	confirmValidator = (control: FormControl): { [s: string]: boolean } => {
-		if (!control.value) {
-			return { required: true };
-		} else if (control.value !== this.projectFrom.controls.password.value) {
-			return { confirm: true, error: true };
-		}
-		return {};
-	};
+            observer.complete();
+          })
+        )
+        .subscribe();
+    });
+  };
 
-	addField(key, value): void {
-		const id =
-			this.controls.length > 0
-				? this.controls[this.controls.length - 1].id + 1
-				: 0;
+  confirmValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.projectFrom.controls.password.value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
 
-		const control = {
-			id,
-			key: `${key || `Field${id}`}`,
-			value: `${value}`,
-			type: "text"
-		};
-		const index = this.controls.push(control);
+  addField(key, value): void {
+    const id =
+      this.controls.length > 0
+        ? this.controls[this.controls.length - 1].id + 1
+        : 0;
 
-		const httpConfigsGroup = this.projectFrom.get([
-			"componentConfigs",
-			"httpParams"
-		]) as FormGroup;
+    const control = {
+      id,
+      key: `${key || `Field${id}`}`,
+      value: `${value}`,
+      type: "text"
+    };
+    const index = this.controls.push(control);
 
-		httpConfigsGroup.addControl(
-			this.controls[index - 1].key,
-			new FormControl(null)
-			//Validators.required
-		);
-	}
+    const httpConfigsGroup = this.projectFrom.get([
+      "componentConfigs",
+      "httpParams"
+    ]) as FormGroup;
 
-	removeField(i: Item, e: MouseEvent): void {
-		e.preventDefault();
-		if (this.controls.length > 0) {
-			const index = this.controls.indexOf(i);
-			this.controls.splice(index, 1);
-			const httpConfigsGroup = this.projectFrom.get([
-				"componentConfigs",
-				"httpParams"
-			]) as FormGroup;
-			httpConfigsGroup.removeControl(i.key);
-		}
-	}
+    httpConfigsGroup.addControl(
+      this.controls[index - 1].key,
+      new FormControl(null)
+      //Validators.required
+    );
+  }
 
-	constructor(
-		private fb: FormBuilder,
-		private _profileService: ProfileService
-	) {}
+  removeField(i: Item, e: MouseEvent): void {
+    e.preventDefault();
+    if (this.controls.length > 0) {
+      const index = this.controls.indexOf(i);
+      this.controls.splice(index, 1);
+      const httpConfigsGroup = this.projectFrom.get([
+        "componentConfigs",
+        "httpParams"
+      ]) as FormGroup;
+      httpConfigsGroup.removeControl(i.key);
+    }
+  }
 
-	ngOnInit() {
-		this.projectFrom = this.fb.group({
-			profile: [null, [Validators.required], [this.titleAsyncValidator]],
-			title: [null, [Validators.required]],
-			type: [null],
-			componentConfigs: this.fb.group({
-				httpRequestUrl: [null],
-				responseData: [null],
-				httpParams: this.fb.group({})
-			})
-		});
+  constructor(
+    private fb: FormBuilder,
+    private _profileService: ProfileService
+  ) {}
 
-		this.selectLoggedInUser$.subscribe(selectLoggedInUser => {
-			if (selectLoggedInUser) {
-				this.projectFrom.patchValue({ ...selectLoggedInUser });
-			}
-		});
+  ngOnInit() {
+    this.projectFrom = this.fb.group({
+      profile: [null, [Validators.required], [this.titleAsyncValidator], ""],
+      title: [null, [Validators.required]],
+      type: [null],
+      componentConfigs: this.fb.group({
+        httpRequestUrl: [null],
+        responseData: [null],
+        httpParams: this.fb.group({})
+      })
+    });
 
-		this.selectProject$.subscribe(selectProject => {
-			if (selectProject) {
-				this.projectFrom.patchValue({ ...selectProject });
-				if (this.controls.length == 0 && selectProject.componentConfigs) {
-					for (let [key, value] of Object.entries(
-						selectProject.componentConfigs.httpParams
-					)) {
-						this.addField(key, value);
-						this.projectFrom.patchValue({ ...selectProject });
-					}
-				}
-			}
-		});
-	}
+    this.selectLoggedInUser$.subscribe(selectLoggedInUser => {
+      if (selectLoggedInUser) {
+        this.projectFrom.patchValue({ ...selectLoggedInUser });
+        if (selectLoggedInUser && selectLoggedInUser.profile) {
+          this.projectFrom.controls.profile.disable();
+        }
+      }
+    });
+
+    this.selectProject$.subscribe(selectProject => {
+      if (selectProject) {
+        this.projectFrom.patchValue({ ...selectProject });
+        if (this.controls.length == 0 && selectProject.componentConfigs) {
+          for (let [key, value] of Object.entries(
+            selectProject.componentConfigs.httpParams
+          )) {
+            this.addField(key, value);
+            this.projectFrom.patchValue({ ...selectProject });
+          }
+        }
+      }
+    });
+  }
 }
