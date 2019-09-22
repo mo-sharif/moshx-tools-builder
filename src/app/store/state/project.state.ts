849f410522118e9952174429a0d10ab00bced6ae
Mo Sharif <mo@mosh-media.com>
2019-09-21 17:42:18 -0700

    Update project.state.ts

--- src/app/store/state/project.state.ts
+++ src/app/store/state/project.state.ts
@@ -4,24 +4,23 @@ import {
 	UiComponents,
 	IComponentConfigs
 } from "../../models/project.interface";
-import { IUser } from "src/app/models/user.interface";
 
 export interface IProjectState {
 	project: IProject;
-	projects: IProject[];
 	selectProject: IProject;
 	container: IProjectContainer[];
-	profile: IUser;
 	UiComponents: UiComponents;
 	updateProject: IComponentConfigs;
 }
 
 export const initialProjectState: IProjectState = {
 	project: null,
-	projects: [],
 	selectProject: null,
 	container: [],
-	profile: null,
-	UiComponents: { showProjectSaveMenu: true, isUserLoggedIn: false, isNewProject: true },
+	UiComponents: {
+		showProjectSaveMenu: true,
+		isUserLoggedIn: false,
+		isNewProject: true
+	},
 	updateProject: null
 };
