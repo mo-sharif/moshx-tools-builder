275f1a55c731a02c371b94806b7e5c9140199ad5
Mo Sharif <mo@mosh-media.com>
2019-09-21 17:42:29 -0700

    Update project.selector.ts

--- src/app/store/selectors/project.selector.ts
+++ src/app/store/selectors/project.selector.ts
@@ -1,28 +1,12 @@
 import { createSelector } from "@ngrx/store";
-
 import { IAppState } from "../state/app.state";
 import { IProjectState } from "../state/project.state";
 
 const selectProjects = (state: IAppState) => state.projects;
 
-export const userProjects = createSelector(
-	selectProjects,
-	(state: IProjectState) => state.projects
-);
-
-export const selectFirstProject = createSelector(
-	selectProjects,
-	(state: IProjectState) => state.projects[0]
-);
-
 export const selectContainers = createSelector(
 	selectProjects,
 	(state: IProjectState) => state.container
-);
-
-export const selectProfile = createSelector(
-	selectProjects,
-	(state: IProjectState) => state.profile
 );
 
 export const selectProject = createSelector(
