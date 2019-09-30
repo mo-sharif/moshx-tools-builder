65c4760706f6b0db505449b80cd8ff221b44510d
Mo Sharif <mo@mosh-media.com>
2019-09-30 08:24:12 -0700

    Update request.service.ts

--- src/app/services/comp/request.service.ts
+++ src/app/services/comp/request.service.ts
@@ -1,33 +1,33 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { of, Observable } from "rxjs";
-import { catchError, switchMap } from "rxjs/operators";
+import { of, Observable, throwError } from "rxjs";
+import { catchError, switchMap, map } from "rxjs/operators";
 
 @Injectable({
 	providedIn: "root"
 })
 export class RequestService {
 	sendPostRequest(url, data): Observable<any> {
-		return this.http.post(url, data)
+		return this.http.post(url, data).pipe(
+			map((res: Response) => res.json()),
+			catchError(this.handleError)
+		);
+	}
 
-/* 			catchError((err: HttpErrorResponse) => {
-				if (err.error instanceof Error) {
-					// A client-side or network error occurred. Handle it accordingly.
-					console.log(`An error occurred: ${err.error.message}`);
-				} else {
-					// The backend returned an unsuccessful response code.
-					// The response body may contain clues as to what went wrong,
-					console.log(
-						`Backend returned code ${err.status}, body was: ${err.error}`
-					);
-				}
+	private handleError(error: HttpErrorResponse) {
+		if (error.error instanceof ErrorEvent) {
+			// A client-side or network error occurred. Handle it accordingly.
+			console.error("An error occurred:", error.error.message);
+		} else {
+			// The backend returned an unsuccessful response code.
+			// The response body may contain clues as to what went wrong,
+			console.error(
+				`Backend returned code ${error.status}, ` + `body was: ${error.error}`
+			);
+		}
+		// return an observable with a user-facing error message
+		return throwError("Something bad happened; please try again later.");
+	}
 
-				// ...optionally return a default fallback value so app can continue (pick one)
-				// which could be a default value
-				// return Observable.of<any>({my: "default value..."});
-				// or simply an empty observable
-				return of(res);
-			}) */
-	};
 	constructor(private http: HttpClient) {}
 }
