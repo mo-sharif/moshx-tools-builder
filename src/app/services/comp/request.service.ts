21c4cf642d79f89de20e48bc375a9bbf9b336e5c
Mo Sharif <mo@mosh-media.com>
2019-09-30 21:53:21 -0700

    Update request.service.ts

--- src/app/services/comp/request.service.ts
+++ src/app/services/comp/request.service.ts
@@ -9,7 +9,6 @@ import { catchError, switchMap, map } from "rxjs/operators";
 export class RequestService {
 	sendPostRequest(url, data): Observable<any> {
 		return this.http.post(url, data).pipe(
-			map((res: Response) => res.json()),
 			catchError(this.handleError)
 		);
 	}
@@ -17,16 +16,13 @@ export class RequestService {
 	private handleError(error: HttpErrorResponse) {
 		if (error.error instanceof ErrorEvent) {
 			// A client-side or network error occurred. Handle it accordingly.
-			console.error("An error occurred:", error.error.message);
+      return throwError(`An error occurred: ${error.error.message}`);
 		} else {
 			// The backend returned an unsuccessful response code.
-			// The response body may contain clues as to what went wrong,
-			console.error(
-				`Backend returned code ${error.status}, ` + `body was: ${error.error}`
-			);
+      // The response body may contain clues as to what went wrong,
+      let message =''
+      return throwError(`${error.status}`);
 		}
-		// return an observable with a user-facing error message
-		return throwError("Something bad happened; please try again later.");
 	}
 
 	constructor(private http: HttpClient) {}
