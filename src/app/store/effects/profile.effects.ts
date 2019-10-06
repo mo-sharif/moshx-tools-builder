23f746d140739a9b687caaf95b9fb36805d99f18
Mo Sharif <mo@mosh-media.com>
2019-10-05 22:17:21 -0700

    Update profile.effects.ts

--- src/app/store/effects/profile.effects.ts
+++ src/app/store/effects/profile.effects.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@angular/core";
 import { of } from "rxjs";
-import { Store } from "@ngrx/store";
+import { Store, select } from "@ngrx/store";
 import { IAppState } from "../state/app.state";
 import { Actions, Effect, ofType } from "@ngrx/effects";
 import { ProfileService } from "../../services/profile/profile.service";
-import { map, switchMap } from "rxjs/operators";
+import { map, switchMap, withLatestFrom, catchError, refCount, publishReplay } from "rxjs/operators";
 import { IProject } from "../../models/project.interface";
 import {
 	GetProfileFromRoute,
@@ -13,6 +13,14 @@ import {
 } from "../actions/profile.actions";
 
 import { UpdateUiComponents } from "../actions/project.actions";
+import {
+	GetUserProfile,
+	EAuthActions,
+	GetUserProfileSuccess
+} from "../actions/auth.actions";
+import { selectLoggedInUser } from "../selectors/auth.selectors";
+import { AddUser, UpdateUser } from "../actions/user.actions";
+import { SetErrorMsg } from "../actions/message.actions";
 
 @Injectable()
 export class ProfileEffects {
@@ -20,6 +28,8 @@ export class ProfileEffects {
 	loadProfileFromRoute$ = this._actions$.pipe(
 		ofType<GetProfileFromRoute>(EProfileActions.GetProfileFromRoute),
 		map(action => action.payload),
+		publishReplay(1),
+        refCount(),
 		switchMap(route => {
 			let profileName = route.replace(".", " ");
 			return this._profileService.loadProfile(profileName).pipe(
@@ -28,6 +38,32 @@ export class ProfileEffects {
 						new GetProfileFromRouteSuccess(projects),
 						new UpdateUiComponents(projects[0].uid)
 					);
+				})
+			);
+		})
+	);
+
+	/*  Responsible for Adding or updating user */
+	@Effect()
+	getUserProfile$ = this._actions$.pipe(
+		ofType<GetUserProfile>(EAuthActions.GetUserProfile),
+		withLatestFrom(this._store.pipe(select(selectLoggedInUser))),
+		publishReplay(1),
+        refCount(),
+		switchMap(([action, userProfile]) => {
+			return this._profileService.getUserProfile(userProfile.uid).pipe(
+				switchMap(user => {
+					if (typeof user === "undefined") {
+						return of(new AddUser(userProfile));
+					} else {
+						if (user.profile) {
+							user.profileSlug = user.profile.replace(/ /g, ".");
+						}
+						return of(new GetUserProfileSuccess(user), new UpdateUser(user));
+					}
+				}),
+				catchError(err => {
+					return of(new SetErrorMsg(`${err}`));
 				})
 			);
 		})
