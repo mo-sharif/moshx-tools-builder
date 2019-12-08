95e46754a00505bba592baa86753ebf5831c208e
Mo Sharif <mo@mosh-media.com>
2019-12-07 23:36:44 -0800

    Create post.state.ts

---
+++ src/store/state/post.state.ts
@@ -0,0 +1,11 @@
+import { IPost } from '../../models/post.interface';
+
+export interface IPostState {
+	posts: IPost[];
+	selectedPost: IPost;
+}
+
+export const initialPostState: IPostState = {
+	posts: null,
+	selectedPost: null
+}
