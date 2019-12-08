5f20fc019cd04c3a4060f0ee16e4e564ab4e8196
Mo Sharif <mo@mosh-media.com>
2019-12-07 23:37:20 -0800

    Create project.reducers.ts

---
+++ src/store/reducers/project.reducers.ts
@@ -0,0 +1,113 @@
+import { EProjectActions } from "../actions/project.actions";
+import { ProjectActions } from "../actions/project.actions";
+import { initialProjectState, IProjectState } from "../state/project.state";
+
+export function projectReducers(
+	state = initialProjectState,
+	action: ProjectActions
+): IProjectState {
+	switch (action.type) {
+		case EProjectActions.GetProject: {
+			return {
+				...state
+			};
+		}
+		case EProjectActions.GetProjects: {
+			return {
+				...state
+			};
+		}
+		case EProjectActions.GetProjectSuccess: {
+			return {
+				...state,
+				selectProject: action.payload
+			};
+		}
+		case EProjectActions.GetSelectedProjectSuccess: {
+			return {
+				...state,
+				selectProject: action.payload
+			};
+		}
+		case EProjectActions.NewProject: {
+			return {
+				...state,
+				selectProject: action.payload,
+				UiComponents: action.payload.UiComponents
+			};
+		}
+		case EProjectActions.NewProjectSuccess: {
+			return {
+				...state,
+				selectProject: action.payload,
+				UiComponents: action.payload.UiComponents
+			};
+		}
+		case EProjectActions.GetContainers: {
+			return {
+				...state,
+				container: action.payload
+			};
+		}
+		case EProjectActions.SaveProject: {
+			return {
+				...state
+			};
+		}
+		case EProjectActions.SaveProjectSuccess: {
+			return {
+				...state,
+				selectProject: action.payload
+			};
+		}
+		case EProjectActions.UpdateProject: {
+			return {
+				...state,
+				updateProject: action.payload
+			};
+		}
+		case EProjectActions.UpdateProjectSuccess: {
+			return {
+				...state,
+				selectProject: action.payload
+			};
+		}
+		case EProjectActions.DeleteProject: {
+			return {
+				...state,
+				selectProject: null
+			};
+		}
+		case EProjectActions.DeleteProjectSuccess: {
+			return {
+				...state
+			};
+		}
+
+		case EProjectActions.UpdateUiComponents: {
+			return {
+				...state
+			};
+		}
+		case EProjectActions.UpdateUiComponentsSuccess: {
+			return {
+				...state,
+				UiComponents: action.payload
+			};
+		}
+		case EProjectActions.GetUserProjects: {
+			return {
+				...state
+			};
+		}
+		case EProjectActions.GetUserProjectsSuccess: {
+			return {
+				...state,
+				userProjects: action.payload
+			};
+		}
+
+		default:
+			return state;
+	}
+}
