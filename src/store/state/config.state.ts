86b405ccb295900a46f574221cad02ec71c76400
Mo Sharif <mo@mosh-media.com>
2019-12-07 23:36:50 -0800

    Create config.state.ts

---
+++ src/store/state/config.state.ts
@@ -0,0 +1,22 @@
+import { IConfig } from "../../models/config.interface";
+import { Configs } from "../../constants/configs";
+
+export interface IConfigState {
+	config: IConfig;
+	isCollapsed: boolean;
+	route: Array<string>;
+	containers: {};
+	urlSegments: any;
+}
+
+export const initialConfigState: IConfigState = {
+	config: {
+		siteName: Configs.SITE_NAME,
+		copyright: Configs.COPYRIGHT,
+		isCollapsed: Configs.IS_COLLAPSED
+	},
+	isCollapsed: Configs.IS_COLLAPSED,
+	route: null,
+	containers: {},
+	urlSegments: null
+};
