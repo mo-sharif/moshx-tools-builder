fa519bdcb4dd3e1d72900b52ab48925e532cd5b7
Mo Sharif <mo@mosh-media.com>
2019-12-07 23:37:26 -0800

    Create config.reducers.ts

---
+++ src/store/reducers/config.reducers.ts
@@ -0,0 +1,58 @@
+import { ConfigActions, EConfigActions } from "../actions/config.actions";
+import { initialConfigState, IConfigState } from "../state/config.state";
+
+export function configReducers(
+	state = initialConfigState,
+	action: ConfigActions
+): IConfigState {
+	switch (action.type) {
+		case EConfigActions.GetConfigSuccess: {
+			return {
+				...state,
+				config: action.payload
+			};
+		}
+		case EConfigActions.OpenDrawer: {
+			return {
+				...state
+			};
+		}
+		case EConfigActions.CloseDrawer: {
+			return {
+				...state
+			};
+		}
+		case EConfigActions.DrawerStatus: {
+			return {
+				...state,
+				config: action.payload
+			};
+		}
+		case EConfigActions.NavigateToRoute: {
+			return {
+				...state,
+				route: action.payload
+			}
+		}
+		case EConfigActions.GetSettings: {
+			return  {
+				...state
+			}
+		}
+		case EConfigActions.GetSettingsSuccess: {
+			return {
+				...state,
+				containers: action.payload 
+			}
+		}
+		case EConfigActions.StoreUrlSegments: {
+			return {
+				...state,
+				urlSegments: action.payload
+			}
+		}
+
+		default:
+			return state;
+	}
+}
