a803ef17e6487e5dc751805b78b3a80f772cbbd7
Mo Sharif <mo@mosh-media.com>
2019-12-07 23:37:10 -0800

    Create message.selectors.ts

---
+++ src/store/selectors/message.selectors.ts
@@ -0,0 +1,10 @@
+import { createSelector } from '@ngrx/store';
+import { IAppState } from '../state/app.state';
+import { IMessageState } from '../state/message.state';
+
+const message = (state: IAppState) => state.message;
+
+export const selectMessage = createSelector(
+    message,
+    (state: IMessageState) => state.message
+)
